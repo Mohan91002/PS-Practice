b9a410974722207d91ea17e05f6995ee1e7a7097
Mohan91002 <mohankumarsatyavarapu225@gmail.com>
2024-01-08 13:07:21 +0530

    date path

--- angularconcepts/src/app/app-routing.module.ts
+++ angularconcepts/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DateComponent } from './date/date.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'date', component:DateComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
